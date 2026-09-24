cd4484d907d7810a0bba5b70982d394b0068c168
Guilherme Rabelo <guirabelo2014@gmail.com>
2026-09-24 19:45:45 -0300

    Ajuste para não debitar do capital quando for resgate

--- gestao-financeira-api/Services/CalculoInvestimentoService.cs
+++ gestao-financeira-api/Services/CalculoInvestimentoService.cs
@@ -48,7 +48,7 @@ namespace GestaoFinanceira.Services
 
                     case TipoMovimentacao.Resgate:
                         resgatado += mov.Valor;
-                        capital -= mov.Valor;
+                        //capital -= mov.Valor;
                         saldo -= mov.Valor;
                         break;
 
